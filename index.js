9e3ab771c01c93ccf610bf8bcf63bf38128206d0
rohanpun4 <159563258+rohanpun4@users.noreply.github.com>
2024-04-06 13:08:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Created by Nim Lal Kandel, C0913081 on 5th April, 2024.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const convertBtn = document.getElementById("convertBtn");
+
+    convertBtn.addEventListener("click", function () {
+        const usdAmount = parseFloat(document.getElementById("number").value);
+
+        const exchangeRate = 1.36;
+
+        const cadAmount = parseFloat(document.getElementById("number1").value);
+        const usdConverted = cadAmount / exchangeRate;
+        
+        if (!isNaN(usdAmount)) {
+            const cadConverted = usdAmount * exchangeRate;
+            document.getElementById("number1").value = cadConverted.toFixed(2);
+        } else if (!isNaN(cadAmount)) {
+            const usdConverted = cadAmount / exchangeRate;
+            document.getElementById("number").value = usdConverted.toFixed(2);
+        }
+    });
+});
